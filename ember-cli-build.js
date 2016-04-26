f4e0e8fe78669d743441a3b23d6bf9f218f18a30
Addy Osmani <addyosmani@gmail.com>
2016-04-26 18:18:49 +0100

    Fingerprinting because..reasons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    fingerprint: {
+      enabled: false
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
