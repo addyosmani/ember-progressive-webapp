cfc5ce6355ceb8e6ed58d4e9e97f8feb5417fc01
Addy Osmani <addyosmani@gmail.com>
2016-04-27 19:28:15 +0100

    experiment with fixing busted sw and .png deploys

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,9 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    // fingerprint: {
-    //   enabled: false
-    // },
+    fingerprint: {
+      enabled: false
+    },
     // ember-cli-inline-content
     inlineContent: {
       'critical-path-css': 'app/styles/app.css'
